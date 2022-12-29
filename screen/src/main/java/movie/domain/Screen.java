d93e75008d516dd037c8e0f6614b4417905c37b0
dhm1022 <dhm1022@naver.com>
2022-12-29 05:48:58 +0000

    screen modify

--- screen/src/main/java/movie/domain/Screen.java
+++ screen/src/main/java/movie/domain/Screen.java
@@ -7,119 +7,78 @@ import java.util.List;
 import lombok.Data;
 import java.util.Date;
 
-
 @Entity
-@Table(name="Screen_table")
+@Table(name = "Screen_table")
 @Data
 
-public class Screen  {
+public class Screen {
 
-
-    
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    
-    
-    
-    
-    
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
-    
-    
-    
-    
-    
+
     private Long movieId;
-    
-    
-    
-    
-    
+
     private String theaterName;
-    
-    
-    
-    
-    
+
     private Date screenDate;
-    
-    
-    
-    
-    
+
     private Integer seatingCapacity;
-    
-    
-    
-    
-    
+
     private Integer seatingRemain;
-    
-    
-    
-    
-    
+
     private Integer seatingPrice;
 
     @PostPersist
-    public void onPostPersist(){
-
+    public void onPostPersist() {
 
         ScreenUpdated screenUpdated = new ScreenUpdated(this);
         screenUpdated.publishAfterCommit();
 
     }
 
-    public static ScreenRepository repository(){
+    public static ScreenRepository repository() {
         ScreenRepository screenRepository = ScreenApplication.applicationContext.getBean(ScreenRepository.class);
         return screenRepository;
     }
 
+    public static void reduceSeating(MovieReserved movieReserved) {
 
+        /**
+         * Example 1: new item
+         * Screen screen = new Screen();
+         * repository().save(screen);
+         * 
+         */
 
-
-    public static void reduceSeating(MovieReserved movieReserved){
-
-        /** Example 1:  new item 
-        Screen screen = new Screen();
-        repository().save(screen);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(movieReserved.get???()).ifPresent(screen->{
-            
-            screen // do something
+        /**
+         * Example 2: finding and process
+         */
+        repository().findById(Long.valueOf(movieReserved.getScreenId())).ifPresent(screen -> {
+            screen.setSeatingRemain(screen.getSeatingRemain() - movieReserved.getSeatingQty());
             repository().save(screen);
+        });
 
-
-         });
-        */
-
-        
-    }
-    public static void addSeating(ReserveCanceled reserveCanceled){
-
-        /** Example 1:  new item 
-        Screen screen = new Screen();
-        repository().save(screen);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(reserveCanceled.get???()).ifPresent(screen->{
-            
-            screen // do something
-            repository().save(screen);
-
-
-         });
-        */
-
-        
     }
 
+    public static void addSeating(ReserveCanceled reserveCanceled) {
+
+        /**
+         * Example 1: new item
+         * Screen screen = new Screen();
+         * repository().save(screen);
+         * 
+         */
+
+        /**
+         * Example 2: finding and process
+         */
+        repository().findById(Long.valueOf(reserveCanceled.getScreenId())).ifPresent(screen -> {
+            screen.setSeatingRemain(screen.getSeatingRemain() + reserveCanceled.getSeatingQty());
+            repository().save(screen);
+        });
+
+    }
 
 }
